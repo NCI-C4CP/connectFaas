92eb6d9a02dfac63e6f90662b7f49bfd817948b3
amber-emmes <ablaylock@emmes.com>
2025-07-15 14:27:00 -0400

    Participant deceased checks for 1370 now also check participantDeceasedNORC flag.

--- utils/sites.js
+++ utils/sites.js
@@ -358,10 +358,14 @@ const updateParticipantData = async (req, res, authObj) => {
             flatDocData[fieldMapping.withdrawConsent] === fieldMapping.yes || 
             flatDocData[fieldMapping.participantMap.destroyData] === fieldMapping.yes ||
             flatDocData[fieldMapping.participantDeceased] === fieldMapping.yes ||
+            flatDocData[fieldMapping.participantDeceasedNORC] === fieldMapping.yes ||
+
             // Check both the existing and incoming data for this
             flatUpdateObj[fieldMapping.withdrawConsent] === fieldMapping.yes || 
             flatUpdateObj[fieldMapping.participantMap.destroyData] === fieldMapping.yes ||
-            flatUpdateObj[fieldMapping.participantDeceased] === fieldMapping.yes
+            flatUpdateObj[fieldMapping.participantDeceased] === fieldMapping.yes ||
+            flatUpdateObj[fieldMapping.participantDeceasedNORC] === fieldMapping.yes
+
         ) {
             if([fieldMapping.initialized, fieldMapping.addressUndeliverable].indexOf(flatDocData[`${fieldMapping.collectionDetails}.${fieldMapping.baseline}.${fieldMapping.bioKitMouthwash}.${fieldMapping.kitStatus}`]) > -1) {
                 flatUpdateObj[`${fieldMapping.collectionDetails}.${fieldMapping.baseline}.${fieldMapping.bioKitMouthwash}.${fieldMapping.kitStatus}`] = FieldValue.delete();
