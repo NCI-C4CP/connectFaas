b270a573a8ede62f84ed748eb7e68581500467e9
Warren Lu <warren.lu6@gmail.com>
2026-03-02 15:56:50 -0500

    initialize survey statuses when verified (#917)
    * initialize survey statuses when verified

--- utils/sites.js
+++ utils/sites.js
@@ -651,18 +651,17 @@ const participantDataCorrection = async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(405).json(getResponseJSON('Only POST requests are accepted!', 405));
     }
-    if(req.body.data.length === 0) return res.status(400).json(getResponseJSON('Bad request. Empty array.', 400));
+    
+    if (!req.body?.data || !Array.isArray(req.body.data) || req.body.data.length === 0) {
+        return res.status(400).json(getResponseJSON('No data in request.', 400));
+    }
 
     try {
         const { updateParticipantCorrection } = require('./firestore');
-        if (req.body.data) {
-            const status = await updateParticipantCorrection(req.body.data[0]);
-            return status === true
-                ? res.status(200).json({ code: 200 })
-                : res.status(400).json(getResponseJSON('Operation Unsuccessful', 400));
-        } else {
-            return res.status(400).json(getResponseJSON('Invalid request format', 400));
-        }
+        const status = await updateParticipantCorrection(req.body.data[0]);
+        return status === true
+            ? res.status(200).json({ code: 200 })
+            : res.status(400).json(getResponseJSON('Operation Unsuccessful', 400));
     } catch (error) {
         console.error(error);
         return res.status(500).json(getResponseJSON('Internal Server Error', 500));
